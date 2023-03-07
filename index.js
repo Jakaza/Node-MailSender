d49be171d61bac2ceab5aea02bec59972ebc27d4
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-03-07 17:01:25 +0000

    Add Mailer

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ app.post('/sendemail', (req, res) => {
 
   if (validateEmail(email)) {
 
+    let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: userEmail,
+      pass: userPass
+    },
+  });
+
+  let info = transporter.sendMail({
+    from: email,
+    to: "goodnessjakazac@gmail.com, germanwise10@gmail.com", 
+    subject: subject,
+    text: content,
+    html: `<p> ${content} </p>`// html body
+  });
+
+    res.status(200).json({
+      messageId : info.messageId,
+      message: 'success'
+    })
 
   } else {
 
