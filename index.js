6488035eceb3e505e07f5918b56630151756d3cb
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-03-07 16:16:48 +0000

    Add router to send email

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
 });
+
+app.post('/sendemail', (req, res)=>{
+
+    const email = req.body.email;
+    const subject = req.body.subject;
+    const content = req.body.content;
+
+    if(validateEmail(email)){
+
+
+      
+    }else{
+
+
+
+      
+    }
+})
+
+function validateEmail(email){
+
+
+
+  return true;
+}
+
+
+
+
 
 app.listen(3000, () => {
   console.log('server started');
